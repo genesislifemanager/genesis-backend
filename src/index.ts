ff2c38c372acda6d2fd09ff77edc379d1482f083
ManupaDev <manupadevsuite@gmail.com>
2023-03-09 14:54:12 +0530

    feat:added packages

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@
 import express, { Request, Response } from "express";
 import { Prisma, PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient();
-
+const cors  = require("cors");
 
 const app = express();
 
 //! Global Midlleware
+app.use(cors());
 app.use(express.json());
 
 //! Mount Routers
