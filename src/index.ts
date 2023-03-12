c3003e73db621e510a2a011fe0981384e0137289
Malmitha <malmitha.20210725@iit.ac.lk>
2023-03-12 16:25:04 +0530

    Made changes to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from "express";
 import { Prisma, PrismaClient } from "@prisma/client";
 import projectRouter from "./Routers/projectRouter";
 import timeblockRouter from "./Routers/timeblockRouter";
+import userRouter from "./Routers/userRouter";
+import ventureRouter from "./Routers/ventureRouter";
 export const prisma = new PrismaClient();
 const cors  = require("cors");
 
@@ -15,6 +17,10 @@ app.use(express.json());
 //! Mount Routers
 app.use("/api/projects",projectRouter);
 app.use("/api/timeblocks",timeblockRouter)
+app.use("/api/users", userRouter);
+app.use("/api/ventures", ventureRouter);
+
+const port = process.env.PORT || 5174
 
 const server = app.listen(5174, () =>
   console.log(`
